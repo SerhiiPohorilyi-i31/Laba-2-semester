96bb3403a5b4496ab3e30cbd35b9c4d1f82f2a23
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2025-02-24 17:58:16 +0200

    Add files via upload
    Lab2_task21.1
    Написати програму розрахунку вартості замовлення пива з двох вкладок TabControl. 
    Вхідні дані для розрахунку – сорт пива (світле, темне), кількість порцій, вартість кожного сорту. 
    Якщо вартість замовлення більше 300, то надається знижка 15%. 
    На першій вкладці розмістити елементи: назви сортів пива - Label, кількість порцій кожного - NumericUpDown, вартість 1 порції - TextBox. 
    На другій вкладці розмістити такі самі елементи для замовлення чіпсів і риби.
    Розмістити на формі контейнер FlowLayoutPanel, в якому помістити кнопки Замовити, Відмінити, Вихід.
    При натисканні кнопки Замовити – обчислити вартість замовлення.
    При натисканні кнопки Відмінити – очистити NumericUpDown.
    При натисканні кнопки Вихід – закрити форму.

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Lab2_task21._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericLightBeer = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericDarkBeer = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericFish = new System.Windows.Forms.NumericUpDown();
+            this.numericChips = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLightBeer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDarkBeer)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericChips)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(861, 352);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.numericLightBeer);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.numericDarkBeer);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(845, 305);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Пиво";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(463, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 31);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "100 (UAH) за порцію";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(463, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 31);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "110 (UAH) за порцію";
+            // 
+            // numericLightBeer
+            // 
+            this.numericLightBeer.Location = new System.Drawing.Point(289, 119);
+            this.numericLightBeer.Name = "numericLightBeer";
+            this.numericLightBeer.Size = new System.Drawing.Size(120, 31);
+            this.numericLightBeer.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Світле пиво (к-сть)";
+            // 
+            // numericDarkBeer
+            // 
+            this.numericDarkBeer.Location = new System.Drawing.Point(289, 65);
+            this.numericDarkBeer.Name = "numericDarkBeer";
+            this.numericDarkBeer.Size = new System.Drawing.Size(120, 31);
+            this.numericDarkBeer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Темне пиво (к-сть)";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.numericFish);
+            this.tabPage2.Controls.Add(this.numericChips);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(845, 305);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Снеки";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox4.Location = new System.Drawing.Point(441, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(218, 31);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "60 (UAH) за порцію";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(441, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 31);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "50 (UAH) за порцію";
+            // 
+            // numericFish
+            // 
+            this.numericFish.Location = new System.Drawing.Point(243, 125);
+            this.numericFish.Name = "numericFish";
+            this.numericFish.Size = new System.Drawing.Size(156, 31);
+            this.numericFish.TabIndex = 3;
+            // 
+            // numericChips
+            // 
+            this.numericChips.Location = new System.Drawing.Point(243, 63);
+            this.numericChips.Name = "numericChips";
+            this.numericChips.Size = new System.Drawing.Size(156, 31);
+            this.numericChips.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Риба (к-сть)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Чіпси (к-сть)";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnOrder);
+            this.flowLayoutPanel1.Controls.Add(this.btnReset);
+            this.flowLayoutPanel1.Controls.Add(this.btnExit);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(283, 416);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(620, 136);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnOrder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOrder.Location = new System.Drawing.Point(3, 3);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(311, 97);
+            this.btnOrder.TabIndex = 0;
+            this.btnOrder.Text = "Сформувати замовлення";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnReset.Location = new System.Drawing.Point(320, 3);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(132, 97);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Відмінити";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(458, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(126, 97);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "До сплати:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(12, 429);
+            this.txtTotal.Multiline = true;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(204, 69);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 674);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLightBeer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDarkBeer)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericChips)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericDarkBeer;
+        private System.Windows.Forms.NumericUpDown numericLightBeer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericFish;
+        private System.Windows.Forms.NumericUpDown numericChips;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_task21._1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnOrder.Click += btnOrder_Click;
+            btnReset.Click += btnReset_Click;
+            btnExit.Click += btnExit_Click;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            numericDarkBeer.Value = 0;
+            numericLightBeer.Value = 0;
+            numericChips.Value = 0;
+            numericFish.Value = 0;
+            txtTotal.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            
+            decimal darkBeerPrice = 110;
+            decimal lightBeerPrice = 100;
+            decimal chipsPrice = 50;
+            decimal fishPrice = 60;
+
+            // Отримання кількості
+            int darkBeerQty = (int)numericDarkBeer.Value;
+            int lightBeerQty = (int)numericLightBeer.Value;
+            int chipsQty = (int)numericChips.Value;
+            int fishQty = (int)numericFish.Value;
+
+            // Розрахунок загальної вартості
+            decimal total = (darkBeerQty * darkBeerPrice) +
+                            (lightBeerQty * lightBeerPrice) +
+                            (chipsQty * chipsPrice) +
+                            (fishQty * fishPrice);
+            if (total > 300)
+            {
+                total *= 0.85m;
+            }
+
+            txtTotal.Text = $"{total:F2} UAH";
+        }
+    }
+}
