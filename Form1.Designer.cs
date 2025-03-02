145b9a89aefcb91ad60787991ed7209b6a61da67
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2025-03-02 19:32:12 +0200

    Add files via upload
    Lab3_task21.1
    Модифікувати форму реєстрації (з заняття 1). 
    Додати на форму вибір року, місяця (числа від 1 до 12), дня народження (від 1 до 31).
    Використати і налаштувати NumericUpDown. 
    Додати на форму перемикач для вибору статі (чолові чи жінка). 
    Усі введені дані вивести в текстове поле.

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Lab3_task21._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.numYear = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numMonth = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numDay = new System.Windows.Forms.NumericUpDown();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прізвище";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Рік народження";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Група";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 563);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Курс";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(151, 54);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(276, 31);
+            this.txtFirstName.TabIndex = 5;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(151, 103);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(276, 31);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.Location = new System.Drawing.Point(151, 497);
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(276, 31);
+            this.txtGroup.TabIndex = 8;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(151, 557);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(276, 31);
+            this.txtCourse.TabIndex = 9;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOK.Location = new System.Drawing.Point(181, 644);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(175, 54);
+            this.btnOK.TabIndex = 10;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnClose.Location = new System.Drawing.Point(832, 644);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(175, 54);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(703, 54);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(415, 534);
+            this.txtOutput.TabIndex = 12;
+            // 
+            // numYear
+            // 
+            this.numYear.Location = new System.Drawing.Point(307, 171);
+            this.numYear.Maximum = new decimal(new int[] {
+            2007,
+            0,
+            0,
+            0});
+            this.numYear.Minimum = new decimal(new int[] {
+            1990,
+            0,
+            0,
+            0});
+            this.numYear.Name = "numYear";
+            this.numYear.Size = new System.Drawing.Size(120, 31);
+            this.numYear.TabIndex = 13;
+            this.numYear.Value = new decimal(new int[] {
+            1990,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Місяць народження";
+            // 
+            // numMonth
+            // 
+            this.numMonth.Location = new System.Drawing.Point(307, 221);
+            this.numMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMonth.Name = "numMonth";
+            this.numMonth.Size = new System.Drawing.Size(120, 31);
+            this.numMonth.TabIndex = 15;
+            this.numMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 272);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(191, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "День народження";
+            // 
+            // numDay
+            // 
+            this.numDay.Location = new System.Drawing.Point(307, 270);
+            this.numDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numDay.Name = "numDay";
+            this.numDay.Size = new System.Drawing.Size(120, 31);
+            this.numDay.TabIndex = 17;
+            this.numDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(54, 47);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(120, 29);
+            this.radioMale.TabIndex = 18;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Чоловік";
+            this.radioMale.UseVisualStyleBackColor = true;
+            this.radioMale.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(243, 47);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(102, 29);
+            this.radioFemale.TabIndex = 19;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Жінка";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Location = new System.Drawing.Point(28, 340);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 118);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Стать";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 766);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numDay);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numMonth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numYear);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Форма реєстрації";
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.NumericUpDown numYear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numMonth;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numDay;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_task21._1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Форма реєстрації"; // заголовок форми
+
+            // Налаштування NumericUpDown контролів
+            numYear.Minimum = 1900;
+            numYear.Maximum = 2007;
+            numMonth.Minimum = 1;
+            numMonth.Maximum = 12;
+            numDay.Minimum = 1;
+            numDay.Maximum = 31;
+
+            // Встановлення текстів для RadioButton контролів
+            radioMale.Text = "Чоловік";
+            radioFemale.Text = "Жінка";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+           
+
+           
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // Перевірка допустимості даних
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtGroup.Text) ||
+                string.IsNullOrWhiteSpace(txtCourse.Text))
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Перевірка року народження
+            int year = (int)numYear.Value;
+            int month = (int)numMonth.Value;
+            int day = (int)numDay.Value;
+
+            // Визначення статі
+            string gender = radioMale.Checked ? "Чоловік" : "Жінка";
+
+            // Виведення даних
+            txtOutput.Text = $"Ім’я: {txtFirstName.Text}\r\n" +
+                             $"Прізвище: {txtLastName.Text}\r\n" +
+                             $"Дата народження: {day}/{month}/{year}\r\n" +
+                             $"Стать: {gender}\r\n" +
+                             $"Група: {txtGroup.Text}\r\n" +
+                             $"Курс: {txtCourse.Text}";
+        }
+    }
+    }
