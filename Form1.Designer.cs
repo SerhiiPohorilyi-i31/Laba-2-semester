bf8dc42abea1f2f6b188360011b08ad9dd5d7cbf
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2025-03-05 23:21:17 +0200

    Add files via upload
    Lab4_task21.1
    Написати програму Улюблені мелодії. 
    В список ComboBox додати 10 назв муз. творів. 
    Вибрані елементи вивести в ListBox.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Lab4_task21._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSelect_Click = new System.Windows.Forms.Button();
+            this.btnClear_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(380, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(528, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 254);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(22, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Оберіть улюблені мелодії:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(523, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ви обрали ці мелодії:";
+            // 
+            // btnSelect_Click
+            // 
+            this.btnSelect_Click.Location = new System.Drawing.Point(43, 314);
+            this.btnSelect_Click.Name = "btnSelect_Click";
+            this.btnSelect_Click.Size = new System.Drawing.Size(156, 68);
+            this.btnSelect_Click.TabIndex = 4;
+            this.btnSelect_Click.Text = "Вибрати";
+            this.btnSelect_Click.UseVisualStyleBackColor = true;
+            this.btnSelect_Click.Click += new System.EventHandler(this.btnSelect_Click_Click);
+            // 
+            // btnClear_Click
+            // 
+            this.btnClear_Click.Location = new System.Drawing.Point(237, 315);
+            this.btnClear_Click.Name = "btnClear_Click";
+            this.btnClear_Click.Size = new System.Drawing.Size(156, 67);
+            this.btnClear_Click.TabIndex = 5;
+            this.btnClear_Click.Text = "Очистити";
+            this.btnClear_Click.UseVisualStyleBackColor = true;
+            this.btnClear_Click.Click += new System.EventHandler(this.btnClear_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 728);
+            this.Controls.Add(this.btnClear_Click);
+            this.Controls.Add(this.btnSelect_Click);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Улюблені мелодії";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelect_Click;
+        private System.Windows.Forms.Button btnClear_Click;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_task21._1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Заповнення списку
+            comboBox1.Items.AddRange(new object[]
+            {
+                "50 Cent - Candy Shop",
+                "Eminem - Lose Yourself",
+                "Future - Mask Off",
+                "Tyga - Ibiza",
+                "Riff Raff - Tip Toe",
+                "YBE - Underdog",
+                "Snoop Dogg - Still D.R.E.",
+                "Ludacris - Act A Fool",
+                "Lil Baby - Band4Band",
+                "Lil Uzi Vert - Do What I Want"
+            });
+            comboBox1.Sorted = true;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelect_Click_Click(object sender, EventArgs e)
+        {
+            string selectedItem = comboBox1.Text;
+            if (!string.IsNullOrEmpty(selectedItem) && !listBox1.Items.Contains(selectedItem))
+            {
+                listBox1.Items.Add(selectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Нічого не вибрано або елемент вже існує в списку.",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);            
+            }
+        }
+
+        private void btnClear_Click_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+
+
+
+    }
+    }
