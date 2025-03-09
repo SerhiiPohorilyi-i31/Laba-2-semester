29dbf361de291c0d4bc6ffca56e4db0a90d0711c
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2025-03-09 16:56:54 +0200

    Add files via upload
    Lab4_task21.2
    Модифікувати форму реєстрації. 
    Замінити вибір місяця  народження на ComboBox. 
    Додати ще один ComboBox для вибору міста народження.

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Lab4_task21._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxCity = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.numYear = new System.Windows.Forms.NumericUpDown();
+            this.numDay = new System.Windows.Forms.NumericUpDown();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прізвище";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(193, 50);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(273, 31);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(193, 108);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(273, 31);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // comboBoxMonth
+            // 
+            this.comboBoxMonth.FormattingEnabled = true;
+            this.comboBoxMonth.Location = new System.Drawing.Point(302, 248);
+            this.comboBoxMonth.Name = "comboBoxMonth";
+            this.comboBoxMonth.Size = new System.Drawing.Size(164, 33);
+            this.comboBoxMonth.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Рік народження";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Місяць народження";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "День народження";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 376);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Місто народження";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 634);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 25);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Група";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 698);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 25);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Курс";
+            // 
+            // comboBoxCity
+            // 
+            this.comboBoxCity.FormattingEnabled = true;
+            this.comboBoxCity.Location = new System.Drawing.Point(298, 376);
+            this.comboBoxCity.Name = "comboBoxCity";
+            this.comboBoxCity.Size = new System.Drawing.Size(168, 33);
+            this.comboBoxCity.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Location = new System.Drawing.Point(56, 444);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 126);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Стать";
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(204, 56);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(102, 29);
+            this.radioFemale.TabIndex = 1;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Жінка";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(17, 56);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(120, 29);
+            this.radioMale.TabIndex = 0;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Чоловік";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.Location = new System.Drawing.Point(193, 628);
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(273, 31);
+            this.txtGroup.TabIndex = 14;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(193, 692);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(273, 31);
+            this.txtCourse.TabIndex = 15;
+            // 
+            // numYear
+            // 
+            this.numYear.Location = new System.Drawing.Point(303, 194);
+            this.numYear.Name = "numYear";
+            this.numYear.Size = new System.Drawing.Size(163, 31);
+            this.numYear.TabIndex = 16;
+            // 
+            // numDay
+            // 
+            this.numDay.Location = new System.Drawing.Point(303, 314);
+            this.numDay.Name = "numDay";
+            this.numDay.Size = new System.Drawing.Size(163, 31);
+            this.numDay.TabIndex = 17;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOK.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnOK.Location = new System.Drawing.Point(133, 767);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(261, 70);
+            this.btnOK.TabIndex = 18;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnClose.Location = new System.Drawing.Point(845, 767);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(261, 70);
+            this.btnClose.TabIndex = 19;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(749, 66);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(464, 657);
+            this.txtOutput.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 922);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.numDay);
+            this.Controls.Add(this.numYear);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBoxCity);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxMonth);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Форма реєстрації";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.ComboBox comboBoxMonth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxCity;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.NumericUpDown numYear;
+        private System.Windows.Forms.NumericUpDown numDay;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_task21._2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Форма реєстрації"; // заголовок форми
+
+            //Налаштування NumericUpDown контролів
+            numYear.Minimum = 1990;
+            numYear.Maximum = 2007;
+            numDay.Minimum = 1;
+            numDay.Maximum = 31;
+
+            // Встановлення текстів для RadioButton
+            radioMale.Text = "Чоловік";
+            radioFemale.Text = "Жінка";
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Заповнення ComboBox з місяцями
+            comboBoxMonth.Items.AddRange(new object[]
+            {
+                "Січня", "Лютого", "Березня", "Квітня", "Травня", "Червня",
+                "Липня", "Серпня", "Вересня", "Жовтня", "Листопада", "Грудня"
+            });
+
+            // Заповнення ComboBox з містами народження
+            comboBoxCity.Items.AddRange(new object[]
+            {
+                "Київ", "Харків", "Одеса", "Дніпро", "Запоріжжя", "Львів",
+                "Кривий Ріг", "Миколаїв", "Черкаси", "Житомир"
+            });
+        }
+
+        private void btnOK_Click_1(object sender, EventArgs e)
+        {
+            // Перевірка допустимості даних
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtGroup.Text) ||
+                string.IsNullOrWhiteSpace(txtCourse.Text) ||
+                comboBoxMonth.SelectedIndex == -1 ||
+                comboBoxCity.SelectedIndex == -1)
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Перевірка року, місяця та дня народження
+            int year = (int)numYear.Value;
+            string month = comboBoxMonth.SelectedItem.ToString();
+            int day = (int)numDay.Value;
+            string city = comboBoxCity.SelectedItem.ToString();
+
+            // Визначення статі
+            string gender = radioMale.Checked ? "Чоловік" : "Жінка";
+
+            // Виведення даних
+            txtOutput.Text = $"Ім’я: {txtFirstName.Text}\r\n" +
+                             $"Прізвище: {txtLastName.Text}\r\n" +
+                             $"Дата народження: {day} {month} {year}\r\n" +
+                             $"Місто народження: {city}\r\n" +
+                             $"Стать: {gender}\r\n" +
+                             $"Група: {txtGroup.Text}\r\n" +
+                             $"Курс: {txtCourse.Text}";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
