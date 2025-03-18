f4fd397d58d5fba10fb7bda45a89b2844d1b8803
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2025-03-18 23:31:21 +0200

    Add files via upload
    Lab5_task21.2
    Реалізувати програму перегляду папок і файлів (можна на одній формі).

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab5_task21._2
+{
+    partial class Провідник
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeViewFolders = new System.Windows.Forms.TreeView();
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeViewFolders
+            // 
+            this.treeViewFolders.Location = new System.Drawing.Point(81, 80);
+            this.treeViewFolders.Name = "treeViewFolders";
+            this.treeViewFolders.Size = new System.Drawing.Size(270, 365);
+            this.treeViewFolders.TabIndex = 0;
+            this.treeViewFolders.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewFolders_AfterSelect);
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.HideSelection = false;
+            this.listViewFiles.Location = new System.Drawing.Point(537, 80);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(270, 365);
+            this.listViewFiles.TabIndex = 1;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.List;
+            // 
+            // Провідник
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 706);
+            this.Controls.Add(this.listViewFiles);
+            this.Controls.Add(this.treeViewFolders);
+            this.Name = "Провідник";
+            this.Text = "Провідник";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewFolders;
+        private System.Windows.Forms.ListView listViewFiles;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_task21._2
+{
+    public partial class Провідник: Form
+    {
+        public Провідник()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Завантаження логічних дисків у TreeView
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                TreeNode rootNode = new TreeNode(drive.Name)
+                {
+                    Tag = drive.RootDirectory.FullName
+                };
+                treeViewFolders.Nodes.Add(rootNode);
+            }
+        }
+
+        private void treeViewFolders_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Відображення файлів у ListView
+            string selectedPath = e.Node.Tag.ToString();
+            listViewFiles.Items.Clear();
+
+            // Завантаження файлів
+            foreach (var file in Directory.GetFiles(selectedPath))
+            {
+                listViewFiles.Items.Add(Path.GetFileName(file));
+            }
+        }
+
+    }
+}
